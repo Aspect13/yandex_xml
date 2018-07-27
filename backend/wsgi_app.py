40af6b5934aa5b5f9b6872818f87bca1998b6b02
Aspect13 <ad13box@gmail.com>
2018-07-27 15:33:16 +0300

    added some flask

--- backend/wsgi_app.py
+++ backend/wsgi_app.py
@@ -1,6 +1,32 @@
 import json
+import os
+
 import xmltodict
 from urllib.parse import parse_qs
+
+STATIC_PATH = 'static'
+
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return "<span style='color:red'>I am app 1 and the request is: {}</span>".format(request.method)
+
+@app.route('/app')
+def the_app():
+	return '<br/>'.join(open(__file__, 'r').readlines())
+
+
+@app.route('/files', methods=['GET'])
+@app.route('/files/<file_name>', methods=['GET'])
+def files(file_name=None):
+	if file_name:
+		return open(STATIC_PATH + '/' + file_name, 'r').read()
+	file_list = os.listdir(STATIC_PATH)
+	return render_template('files.html', files=file_list)
+
 
 
 def convert(xml_file, xml_attribs=True):
@@ -24,3 +50,7 @@ def application(env, start_response):
 
 	start_response('200 OK', [('Content-Type', 'text/html')])
 	return b"DAAAAAAM son where'd u got this?"
+
+
+if __name__ == '__main__':
+	app.run(port=2222, debug=True)
